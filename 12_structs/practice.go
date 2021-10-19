d2304a17a0a8f7b65e17eb38e1be6cd12a962785
Florencia Caballero <fcaballero@proofpoint.com>
2021-10-19 12:35:14 -0300

    practice with structs and interfaces

---
+++ 12_structs/practice.go
@@ -0,0 +1,11 @@
+package main
+
+// A struct in Golang is a user-defined type which allows to group/combine items of possibly different types into a single type
+
+type User struct {
+	name, email, password string
+}
+
+func PracticeStructs() {
+
+}
--- 13_interfaces/main.go
+++ 13_interfaces/main.go
@@ -2,38 +2,115 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strings"
+	"unicode"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
+// Helper Functions
+func verifyPassword(password string) error {
+	var uppercasePresent bool
+	var lowercasePresent bool
+	var numberPresent bool
+	var specialCharPresent bool
+	const minPassLength = 8
+	const maxPassLength = 64
+	var passLen int
+	var errorString string
+
+	for _, ch := range password {
+		switch {
+		case unicode.IsNumber(ch):
+			numberPresent = true
+			passLen++
+		case unicode.IsUpper(ch):
+			uppercasePresent = true
+			passLen++
+		case unicode.IsLower(ch):
+			lowercasePresent = true
+			passLen++
+		case unicode.IsPunct(ch) || unicode.IsSymbol(ch):
+			specialCharPresent = true
+			passLen++
+		case ch == ' ':
+			passLen++
+		}
+	}
+	appendError := func(err string) {
+		if len(strings.TrimSpace(errorString)) != 0 {
+			errorString += ", " + err
+		} else {
+			errorString = err
+		}
+	}
+	if !lowercasePresent {
+		appendError("lowercase letter missing")
+	}
+	if !uppercasePresent {
+		appendError("uppercase letter missing")
+	}
+	if !numberPresent {
+		appendError("atleast one numeric character required")
+	}
+	if !specialCharPresent {
+		appendError("special character missing")
+	}
+	if !(minPassLength <= passLen && passLen <= maxPassLength) {
+		appendError(fmt.Sprintf("password length must be between %d to %d characters long", minPassLength, maxPassLength))
+	}
+
+	if len(errorString) != 0 {
+		return fmt.Errorf(errorString)
+	}
+	return nil
+}
+
 // Define interface
-type Shape interface {
-	area() float64
+type User interface {
+	Authorized() int
+	ValidatePassword(string) error
 }
 
-type Circle struct {
-	x, y, radius float64
+type Admin struct {
+	Name  string
+	Email string
 }
 
-type Rectangle struct {
-	width, height float64
+type AdminWithPassword struct {
+	*Admin
+	Password string
 }
 
-func (c Circle) area() float64 {
-	return math.Pi * c.radius * c.radius
+func (this Admin) Authorized() int {
+	return 5
 }
 
-func (r Rectangle) area() float64 {
-	return r.width * r.height
+func (this *AdminWithPassword) ValidatePassword(password string) error {
+	errValidatePassword := verifyPassword(password)
+	if errValidatePassword == nil {
+		bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+		if err != nil {
+			return err
+		}
+		this.Password = string(bytes)
+		return nil
+	} else {
+		return errValidatePassword
+	}
 }
 
-func getArea(s Shape) float64 {
-	return s.area()
+func Auth(user User, password string) bool {
+	if user.Authorized() > 4 && user.ValidatePassword(password) == nil {
+		return true
+	}
+	return false
 }
 
 func main() {
-	circle := Circle{x: 0, y: 0, radius: 5}
-	rectangle := Rectangle{width: 10, height: 5}
+	admin := Admin{"Admin", "admin@mail.com"}
+	adminWpassdors := AdminWithPassword{&admin, ""}
+	password := "Apple"
 
-	fmt.Printf("Circle Area: %f\n", getArea(circle))
-	fmt.Printf("Rectangle Area: %f\n", getArea(rectangle))
+	fmt.Println(Auth(&adminWpassdors, password))
 }
