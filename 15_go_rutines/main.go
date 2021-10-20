d0e6ac263819c5c01d2550972ccd4e85e6265da8
Florencia Caballero <fcaballero@proofpoint.com>
2021-10-20 18:21:08 -0300

    go rutines and go channels

--- 15_go_rutines/main.go
+++ 15_go_rutines/main.go
@@ -6,31 +6,52 @@ import (
 	"time"
 )
 
-func splitName(name string) {
+func sync(name string) {
 	letters := strings.Split(name, "")
-
+	fmt.Println("Series 1")
 	for _, value := range letters {
-		time.Sleep(1000 * time.Millisecond)
+		time.Sleep(1 * time.Second)
 		fmt.Println(value)
 	}
-}
-
-func sync(name string) {
-	splitName(name)
-	fmt.Println("Finished!")
+	fmt.Println("Series 2")
 }
 
 func conc(name string) {
-	go splitName(name)
-	fmt.Println("Finished!")
-	var wait string
-	fmt.Scanln(&wait)
+	letters := strings.Split(name, "")
+	fmt.Println("Series 1")
+	go func() {
+		for _, value := range letters {
+			fmt.Println(value)
+			time.Sleep(1 * time.Second)
+		}
+		fmt.Println("Done go routine")
+	}()
+	fmt.Println("Series 2")
+	time.Sleep(1 * time.Second * time.Duration(len(letters)))
+}
+
+func channel(name string) {
+	letters := strings.Split(name, "")
+	done := make(chan bool)
+	fmt.Println("Series 1")
+	go func() {
+		for _, value := range letters {
+			fmt.Println(value)
+			time.Sleep(1 * time.Second)
+		}
+		fmt.Println("Done go routine")
+		done <- true
+	}()
+	fmt.Println("Series 2")
+	<-done
 }
 
 func main() {
 	name := "Florencia"
 	fmt.Println("Synchronous")
 	sync(name)
-	fmt.Println("Concurrent")
+	fmt.Println("Concurrent Go routine")
 	conc(name)
+	fmt.Println("Concurrent Go routine and go channel")
+	channel(name)
 }
