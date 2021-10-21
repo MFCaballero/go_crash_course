570383022cd2f852c6da7a74af21ee3b4f7c5b9f
Florencia Caballero <fcaballero@proofpoint.com>
2021-10-21 19:04:54 -0300

    web services

--- 14_web/main.go
+++ 14_web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -13,9 +14,27 @@ func about(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>About</h1>")
 }
 
+type Course struct {
+	Title  string `json:"title"`
+	Videos int    `json:"videos"`
+}
+
+type Courses []Course
+
+func handleJson(w http.ResponseWriter, r *http.Request) {
+	courses := Courses{
+		Course{"Go", 100},
+		Course{"Python", 90},
+		Course{"NodeJS", 95},
+	}
+	json.NewEncoder(w).Encode(courses)
+}
+
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/about", about)
+	http.HandleFunc("/json", handleJson)
 	fmt.Println("Server Starting...")
+
 	http.ListenAndServe(":3000", nil)
 }
---
+++ 14_web_microservices/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/MFCaballero/go_crash_course/14_web_microservices/handlers"
+)
+
+func main() {
+	fmt.Println("Server Starting...")
+
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
+	s := &http.Server{
+		Addr:         ":3000",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	timeOutContext, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(timeOutContext)
+}
